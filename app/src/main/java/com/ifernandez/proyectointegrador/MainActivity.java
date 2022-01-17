f62c9e20963e6779e9dc33b69b6f47af42a1e113
Alejandro Rojas Ruiz <alejandrorojasruiz7@gmail.com>
2022-01-17 12:58:41 +0100

    Added getWeekDateList methods

--- app/src/main/java/com/ifernandez/proyectointegrador/MainActivity.java
+++ app/src/main/java/com/ifernandez/proyectointegrador/MainActivity.java
@@ -4,11 +4,72 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int week;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar today = Calendar.getInstance();
+        week = today.get(Calendar.DAY_OF_WEEK);
+    }
+
+
+    /**
+     * This method retrieves all the days of the current week plus the specified number of weeks
+     * @param plusWeek Number of weeks plus or minus the current, you want to retrieve the days for
+     * @return a List of "Date" with all the days of the current week
+     */
+    public static List<Date> getWeekDateList(int plusWeek) {
+        Calendar cal = Calendar.getInstance();
+        if (plusWeek != 0){
+            cal.add(Calendar.DAY_OF_MONTH,7*plusWeek);
+        }
+        //  Set the first day of the week. According to Spanish custom, the first day of the week is Monday
+        cal.setFirstDayOfWeek(Calendar.MONDAY);
+        //  Get the current date is the day of the week
+        int dayWeek = cal.get(Calendar.DAY_OF_WEEK);
+        if(dayWeek==1){
+            dayWeek = 8;
+        }
+
+        //  According to the rules of the calendar, subtract the difference between the day of the week and the first day of the week from the current date
+        cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - dayWeek);
+        Date mondayDate = cal.getTime();
+
+        cal.add(Calendar.DATE, 4 +cal.getFirstDayOfWeek());
+        Date sundayDate = cal.getTime();
+
+        List lDate = new ArrayList();
+        lDate.add(mondayDate);
+        Calendar calBegin = Calendar.getInstance();
+        //  Set the time of this Calendar with the given Date
+        calBegin.setTime(mondayDate);
+        Calendar calEnd = Calendar.getInstance();
+        //  Set the time of this Calendar with the given Date
+        calEnd.setTime(sundayDate);
+        //Test whether this date is after the specified date
+        while (sundayDate.after(calBegin.getTime())) {
+            //  According to the rules of the calendar, add or subtract a specified amount of time for a given calendar field
+            calBegin.add(Calendar.DAY_OF_MONTH, 1);
+            lDate.add(calBegin.getTime());
+        }
+        return lDate;
+    }
+
+    /**
+     * This method retrieves all the days of the current week in date format
+     * @return a List of "Date" with all the days of the current week
+     */
+    public static List<Date> getWeekDateList() {
+        return getWeekDateList(0);
     }
 }
