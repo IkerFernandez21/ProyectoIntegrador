c2ded06d5b473a7f2c583060fe29b437e2c0ecf5
Alejandro Rojas Ruiz <alejandrorojasruiz7@gmail.com>
2022-01-18 09:26:06 +0100

    RecyclerViews adjusted and working correctly

--- app/src/main/java/com/ifernandez/proyectointegrador/MainActivity.java
+++ app/src/main/java/com/ifernandez/proyectointegrador/MainActivity.java
@@ -1,8 +1,13 @@
 package com.ifernandez.proyectointegrador;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -11,23 +16,150 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int week;
+    private int showingWeek;
+    private RecyclerView rvMonday;
+    private RecyclerView rvTuesday;
+    private RecyclerView rvWednesday;
+    private RecyclerView rvThursday;
+    private RecyclerView rvFriday;
+    private RecyclerView rvSaturday;
+    private RecyclerView rvSunday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Calendar today = Calendar.getInstance();
-        week = today.get(Calendar.DAY_OF_WEEK);
+        showingWeek = 0;
+        setDaysOfWeekUI();
+        setRecyclersUp();
+    }
+
+    /**
+     * Set ups RecyclerViews on init
+     */
+    private void setRecyclersUp(){
+
+        ArrayList<String> list = new ArrayList();
+        list.add("ejemplo 1 muy largo super largo hiper mega super larguisimo de la muerte");
+        list.add("ejemplo 2");
+        list.add("ejemplo 3");
+        list.add("ejemplo 4");
+        list.add("ejemplo 5");
+        list.add("ejemplo 6");
+        list.add("ejemplo 7");
+        list.add("ejemplo 8");
+        list.add("ejemplo 9");
+
+
+        //Recycler Monday
+        rvMonday = findViewById(R.id.rv_monday);
+        LinearLayoutManager mLayout = new LinearLayoutManager(this);
+        rvMonday.setLayoutManager(mLayout);
+        RecyclerView.Adapter adapter = new MiAdaptador(this,list);
+        rvMonday.setAdapter(adapter);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rvMonday.getContext(),
+                mLayout.getOrientation());
+        rvMonday.addItemDecoration(dividerItemDecoration);
+
+        //Recycler Tuesday
+        rvTuesday = findViewById(R.id.rv_tueday);
+        mLayout = new LinearLayoutManager(this);
+        rvTuesday.setLayoutManager(mLayout);
+        adapter = new MiAdaptador(this,list);
+        rvTuesday.setAdapter(adapter);
+
+        dividerItemDecoration = new DividerItemDecoration(rvTuesday.getContext(),
+                mLayout.getOrientation());
+        rvTuesday.addItemDecoration(dividerItemDecoration);
+
+        //Recycler Wednesday
+        rvWednesday = findViewById(R.id.rv_wednesday);
+        mLayout = new LinearLayoutManager(this);
+        rvWednesday.setLayoutManager(mLayout);
+        adapter = new MiAdaptador(this,list);
+        rvWednesday.setAdapter(adapter);
+
+        dividerItemDecoration = new DividerItemDecoration(rvWednesday.getContext(),
+                mLayout.getOrientation());
+        rvWednesday.addItemDecoration(dividerItemDecoration);
+
+        //Recycler Thursday
+        rvThursday = findViewById(R.id.rv_thursday);
+        mLayout = new LinearLayoutManager(this);
+        rvThursday.setLayoutManager(mLayout);
+        adapter = new MiAdaptador(this,list);
+        rvThursday.setAdapter(adapter);
+
+        dividerItemDecoration = new DividerItemDecoration(rvThursday.getContext(),
+                mLayout.getOrientation());
+        rvThursday.addItemDecoration(dividerItemDecoration);
+
+        //Recycler Friday
+        rvFriday = findViewById(R.id.rv_friday);
+        mLayout = new LinearLayoutManager(this);
+        rvFriday.setLayoutManager(mLayout);
+        adapter = new MiAdaptador(this,list);
+        rvFriday.setAdapter(adapter);
+
+        dividerItemDecoration = new DividerItemDecoration(rvFriday.getContext(),
+                mLayout.getOrientation());
+        rvFriday.addItemDecoration(dividerItemDecoration);
+
+        //Recycler Saturday
+        rvSaturday = findViewById(R.id.rv_saturday);
+        mLayout = new LinearLayoutManager(this);
+        rvSaturday.setLayoutManager(mLayout);
+        adapter = new MiAdaptador(this,list);
+        rvSaturday.setAdapter(adapter);
+
+        dividerItemDecoration = new DividerItemDecoration(rvSaturday.getContext(),
+                mLayout.getOrientation());
+        rvSaturday.addItemDecoration(dividerItemDecoration);
+
+        //Recycler Sunday
+        rvSunday = findViewById(R.id.rv_sunday);
+        mLayout = new LinearLayoutManager(this);
+        rvSunday.setLayoutManager(mLayout);
+        adapter = new MiAdaptador(this,list);
+        rvSunday.setAdapter(adapter);
+
+        dividerItemDecoration = new DividerItemDecoration(rvSunday.getContext(),
+                mLayout.getOrientation());
+        rvSunday.addItemDecoration(dividerItemDecoration);
     }
 
 
     /**
+     * This method set the days of the week in the UI
+     */
+    private void setDaysOfWeekUI(){
+
+        TextView monday = findViewById(R.id.tv_monday);
+        TextView tuesday = findViewById(R.id.tv_tuesday);
+        TextView wednesday = findViewById(R.id.tv_wednesday);
+        TextView thursday = findViewById(R.id.tv_thursday);
+        TextView friday = findViewById(R.id.tv_friday);
+        TextView weekend = findViewById(R.id.tv_weekend);
+
+        List<Date> showingWeekList = getWeekDateList(showingWeek);
+
+        monday.setText("Lunes " + showingWeekList.get(0).getDate());
+        tuesday.setText("Martes " + showingWeekList.get(1).getDate());
+        wednesday.setText("Miercoles " + showingWeekList.get(2).getDate());
+        thursday.setText("Jueves " + showingWeekList.get(3).getDate());
+        friday.setText("Viernes " + showingWeekList.get(4).getDate());
+        weekend.setText("Sabado/domingo " + showingWeekList.get(5).getDate() + "/" + showingWeekList.get(6).getDate());
+
+    }
+
+    /**
      * This method retrieves all the days of the current week plus the specified number of weeks
      * @param plusWeek Number of weeks plus or minus the current, you want to retrieve the days for
-     * @return a List of "Date" with all the days of the current week
+     * @return a List of "Date" with all the days of the specified week
      */
+    @NonNull
     public static List<Date> getWeekDateList(int plusWeek) {
         Calendar cal = Calendar.getInstance();
         if (plusWeek != 0){
@@ -69,6 +201,7 @@ public class MainActivity extends AppCompatActivity {
      * This method retrieves all the days of the current week in date format
      * @return a List of "Date" with all the days of the current week
      */
+    @NonNull
     public static List<Date> getWeekDateList() {
         return getWeekDateList(0);
     }
---
+++ app/src/main/java/com/ifernandez/proyectointegrador/MiAdaptador.java
@@ -0,0 +1,83 @@
+package com.ifernandez.proyectointegrador;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MiAdaptador extends RecyclerView.Adapter<MiAdaptador.ViewHolder> {
+    private List<String> mData;
+    private LayoutInflater mInflater;
+
+    private int pos = 0;
+    public int getPos(){return this.pos;}
+    public void decrementarPos(){this.pos --;};
+
+    MiAdaptador(Context context, List<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String animal = mData.get(position);
+        holder.myTextView.setText(animal);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                TextView myTV = view.findViewById(R.id.tvTareaNombre);
+                ColorDrawable viewColor = (ColorDrawable) myTV.getBackground();
+
+                if (viewColor == null) myTV.setBackgroundColor(Color.RED);
+                else{
+                    int colorId = viewColor.getColor();
+                    if (colorId == Color.WHITE) myTV.setBackgroundColor(Color.RED);
+                    else myTV.setBackgroundColor(Color.WHITE);
+                }
+
+                pos = position;
+            }
+        }
+        );
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View view){
+                TextView myTV = view.findViewById(R.id.tvTareaNombre);
+                myTV.setBackgroundColor(Color.BLUE);
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount(){
+        return mData.size();
+    }
+
+    public  class ViewHolder extends RecyclerView.ViewHolder{
+        TextView myTextView;
+
+        ViewHolder(View itemView){
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.tvTareaNombre);
+        }
+    }
+}
