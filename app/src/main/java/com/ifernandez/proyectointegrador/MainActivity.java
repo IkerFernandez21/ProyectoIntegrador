c5c333a9f78779170739d6bb9e3ed2daa800152d
IkerFernandez21 <Ikeriratxe2003>
2022-02-03 13:05:18 +0100

    actualizacion splash

--- app/src/main/java/com/ifernandez/proyectointegrador/MainActivity.java
+++ app/src/main/java/com/ifernandez/proyectointegrador/MainActivity.java
@@ -22,9 +22,13 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.GestureDetector;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -416,4 +420,5 @@ public class MainActivity extends AppCompatActivity {
         i.putExtra("dayDate", week.get(day));
         activityResultLauncher.launch(i);
     }
+
 }
